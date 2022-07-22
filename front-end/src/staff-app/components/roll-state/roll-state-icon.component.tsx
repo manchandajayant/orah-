e5a7df30a404c860357a4dc0500e74a896fbafa9
manchandajayant <maanjayant@gmail.com>
2022-07-22 15:25:49 +0530

    task 2 complete, refator remains to be done

--- front-end/src/staff-app/components/roll-state/roll-state-icon.component.tsx
+++ front-end/src/staff-app/components/roll-state/roll-state-icon.component.tsx
@@ -4,14 +4,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { BorderRadius } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { RolllStateType } from "shared/models/roll"
-
+import {Person} from "shared/models/person"
 interface Props {
   type: RolllStateType
   size?: number
   onClick?: () => void
+  student?:Person
 }
 export const RollStateIcon: React.FC<Props> = (props) => {
-  const { type, size = 20, onClick } = props
+  const { type, size = 20, onClick, student } = props
   return (
     <S.Icon size={size} border={type === "unmark"} bgColor={getBgColor(type)} clickable={Boolean(onClick)} onClick={onClick}>
       <FontAwesomeIcon icon="check" size={size > 14 ? "lg" : "sm"} />
--- front-end/src/staff-app/components/roll-state/roll-state-switcher.component.tsx
+++ front-end/src/staff-app/components/roll-state/roll-state-switcher.component.tsx
@@ -1,19 +1,23 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext,useEffect } from "react"
 import { RolllStateType } from "shared/models/roll"
 import { RollStateIcon } from "staff-app/components/roll-state/roll-state-icon.component"
-import {RollContext} from "Context/student-context-api"
-import {Person} from "shared/models/person"
+import { RollContext } from "Context/student-context-api"
+import { Person } from "shared/models/person"
 interface Props {
   initialState?: RolllStateType
   size?: number
   onStateChange?: (newState: RolllStateType) => void
-  student:Person
+  student: Person
 }
-export const RollStateSwitcher: React.FC<Props> = ({ initialState = "unmark", size = 40, onStateChange,student }) => {
-  const cstate = useContext(RollContext)
-  const {loadState,state,dispatch} = useContext(RollContext)
-  const [rollState, setRollState] = useState(initialState)
+export const RollStateSwitcher: React.FC<Props> = ({ initialState = "unmark", size = 40, onStateChange, student }) => {
+  const { loadState, state, dispatch } = useContext(RollContext)
+  // Set right data type
+  const [rollState, setRollState] = useState<any>(initialState)
 
+  useEffect(() => {
+    setRollState(student.rollValue)
+  }, [student])
+  
   const nextState = () => {
     const states: RolllStateType[] = ["present", "late", "absent"]
     if (rollState === "unmark" || rollState === "absent") return states[0]
@@ -24,11 +28,11 @@ export const RollStateSwitcher: React.FC<Props> = ({ initialState = "unmark", si
   const onClick = () => {
     const next = nextState()
     setRollState(next)
-    dispatch({type:"student_rolls",payload:{student_id:student.id , roll_state: next}})
+    dispatch({ type: "student_rolls", payload: { student_id: student.id, roll_state: next } })
     if (onStateChange) {
       onStateChange(next)
     }
   }
 
-  return <RollStateIcon type={rollState} size={size} onClick={onClick} />
+  return <RollStateIcon type={rollState} size={size} onClick={onClick} student={student}/>
 }
