cfea0c0c405964631997e9290856038820ba45e9
manchandajayant <maanjayant@gmail.com>
2022-07-24 20:31:57 +0530

    refactored dropdown component to be uniform with other components

--- front-end/src/staff-app/components/dropdown-sort/dropdown.tsx
+++ front-end/src/staff-app/components/dropdown-sort/dropdown.tsx
@@ -2,51 +2,6 @@ import React, { useState } from "react"
 import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { sortAction } from "staff-app/daily-care/home-board.page"
-const Main = styled("div")`
-    font-family: sans-serif;
-`
-const DropDownContainer = styled("div")`
-    width: 10.5em;
-    margin: 0 auto;
-`
-
-const DropDownHeader = styled("div")`
-    padding: 0.5em 1em 0.4em 1em;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
-    font-weight: 500;
-    font-size: 0.8rem;
-    display: flex;
-    justify-content: space-between;
-    background: #ffffff;
-    cursor: pointer;
-`
-
-const DropDownListContainer = styled("div")`
-    position: fixed;
-    width: inherit;
-    cursor: pointer;
-`
-
-const DropDownList = styled("div")`
-    padding: 0;
-    margin: 0;
-    background: #ffffff;
-    border: 2px solid #e5e5e5;
-    box-sizing: border-box;
-    font-size: 0.8rem;
-    font-weight: 500;
-`
-
-const ListItem = styled("div")`
-    padding: 0.8em 0 0.8em 0.8em;
-    &:hover {
-        background-color: rgb(247, 247, 247);
-    }
-`
-
-const IconContainer = styled("span")`
-    padding-top: 1.3px;
-`
 
 const options = ["first", "last"]
 
@@ -67,28 +22,71 @@ export const DropDownComponent: React.FC<DropDownProps> = ({ onClickSort }) => {
     }
 
     return (
-        <Main>
-            <DropDownContainer>
-                <DropDownHeader onClick={toggling}>
+        <S.Main>
+            <S.DropDownContainer>
+                <S.DropDownHeader onClick={toggling}>
                     <span>{selectedOption || "First Name"}</span>
-                    <IconContainer>
+                    <S.IconContainer>
                         <FontAwesomeIcon icon={"angle-down"} />
-                    </IconContainer>
-                </DropDownHeader>
+                    </S.IconContainer>
+                </S.DropDownHeader>
                 {isOpen && (
-                    <DropDownListContainer>
-                        <DropDownList>
+                    <S.DropDownListContainer>
+                        <S.DropDownList>
                             {options.map((option: any) => (
-                                <ListItem key={Math.random()} onClick={() => onOptionClicked(option)}>
+                                <S.ListItem key={Math.random()} onClick={() => onOptionClicked(option)}>
                                     {option === "first" ? "First Name" : "Last Name"}
-                                </ListItem>
+                                </S.ListItem>
                             ))}
-                        </DropDownList>
-                    </DropDownListContainer>
+                        </S.DropDownList>
+                    </S.DropDownListContainer>
                 )}
-            </DropDownContainer>
-        </Main>
+            </S.DropDownContainer>
+        </S.Main>
     )
+}
+
+const S = {
+    Main: styled.div`
+        font-family: sans-serif;
+    `,
+    DropDownContainer: styled.div`
+        width: 10.5em;
+        margin: 0 auto;
+    `,
+    DropDownHeader: styled.div`
+        padding: 0.5em 1em 0.4em 1em;
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+        font-weight: 500;
+        font-size: 0.8rem;
+        display: flex;
+        justify-content: space-between;
+        background: #ffffff;
+        cursor: pointer;
+    `,
+    DropDownListContainer: styled.div`
+        position: fixed;
+        width: inherit;
+        cursor: pointer;
+    `,
+    DropDownList: styled.div`
+        padding: 0;
+        margin: 0;
+        background: #ffffff;
+        border: 2px solid #e5e5e5;
+        box-sizing: border-box;
+        font-size: 0.8rem;
+        font-weight: 500;
+    `,
+    ListItem: styled.div`
+        padding: 0.8em 0 0.8em 0.8em;
+        &:hover {
+            background-color: rgb(247, 247, 247);
+        }
+    `,
+    IconContainer: styled.span`
+        padding-top: 1.3px;
+    `,
 }
 
 export default DropDownComponent
